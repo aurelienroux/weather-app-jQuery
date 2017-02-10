39a6ea000d7608adf25773d266dcb4f753e569cd
Aurelien Roux <aureroux13@me.com>
2017-02-10 17:08:44 -0500

    add a document ready function

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
-var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
-var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+$(function(){
 
-var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
-var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+	var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
+	var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
+	var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+	var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
+	var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 
-function getCoordinatesForCity(cityName){
-	var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+	function getCoordinatesForCity(cityName){
+		var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
 
-	$.getJSON(url, function(response){
-		console.log(response.results[0].geometry.location);
-		// return (response.results[0].geometry.location);
+		$.getJSON(url, function(locResponse){
+			// console.log(locResponse.results[0].geometry.location);
+			return (locResponse.results[0].geometry.location);
+		})
+	}
+
+
+	function getCurrentWeather(coords){
+		var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
+
+		$.getJSON(url, function(WeatherResponse){
+			console.log(WeatherResponse);
+		})
+	}
+
+	$('form').submit(function(evt){
+		evt.preventDefault();
+		var city = $('.city-input').val();
+
+		getCoordinatesForCity(city);
+
+		$('.city-weather').html('test');
 	})
-}
 
-
-function getCurrentWeather(coords){
-	var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
-
-	$.getJSON(url, function(response){
-		console.log(response)
-	})
-}
-
-
-// $('form').submit(function(evt){
-// 	evt.preventDefault();
-// 	var city = $('.city-input').val();
-
-// 	getCoordinatesForCity(city);
-		// $('.city-weather').html('test');
-// })
-
-
+});
 
 
