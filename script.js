55a7032b68dac981d7e150e1489bfe718d9489a7
Aurelien Roux <aureroux13@me.com>
2017-02-10 18:37:06 -0500

    chain functions

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-$(function(){
+$(document).ready(function(){
 
 	var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
 	var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
 	var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-
 	var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
 	var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-
 	function getCoordinatesForCity(cityName){
 		var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
-
-		$.getJSON(url, function(locResponse){
-			// console.log(locResponse.results[0].geometry.location);
+		return $.getJSON(url)
+		.then(locResponse => {
 			return (locResponse.results[0].geometry.location);
 		})
 	}
 
-
 	function getCurrentWeather(coords){
 		var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
-
-		$.getJSON(url, function(WeatherResponse){
-			console.log(WeatherResponse);
-		})
+		return $.getJSON(url)
 	}
 
-	$('form').submit(function(evt){
+	$('form').on('submit', function(evt){
 		evt.preventDefault();
 		var city = $('.city-input').val();
-
-		getCoordinatesForCity(city);
-
-		$('.city-weather').html('test');
+		getCoordinatesForCity(city)
+		.then(getCurrentWeather)
+		.then( data => $('.city-weather').html(data.currently.temperature)); 
 	})
-
 });
 
 
