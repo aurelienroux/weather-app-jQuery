2763381e154a1204e90e62a37966ab771a030014
Aurelien Roux <aureroux13@me.com>
2017-02-10 16:08:01 -0500

    get all API keys

---
+++ script.js
@@ -0,0 +1,6 @@
+var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
+var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
+var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
+var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
